bdb740170a0f9e5599c272e64fce324db102c3d7
OGKevin <Kevinhellemun@icloud.com>
2017-02-23 15:38:15 +0100

    Does what is asked from FCC

--- jss/main.js
+++ jss/main.js
@@ -6,37 +6,20 @@ $(function() {
         resultsURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm;
         $("#results").empty();
         getJson();
-        console.log(searchTerm);
     });
 
     function getJson() {
         $.ajax({
                 url: resultsURL,
                 async: 'false',
-                // type: 'default GET (Other values: POST)',
                 dataType: 'json',
-                // data: {
-                //     param1: 'value1'
-                // }
             })
             .done(function(data) {
-                // $("#title").html(data[1][0]);
-                // $("#discription").html(data[2][0]);
-                // $("#link").attr('href', data[3][0]);
                 for (var i = 0; i < data[1].length; i++) {
-
                     $("#results").append('<a target="_blank" href=' + data[3][i] + '> <h3>' + data[1][i] + '</h3> <p> ' + data[2][i] + '</p> </a>')
-                    console.log("success");
-                    console.log(data);
                 }
             })
-            .fail(function() {
-                console.log("error");
-            })
-            .always(function() {
-                console.log("complete");
-            });
-
-
+            .fail(function() {})
+            .always(function() {});
     };
 })
